b8b6787e15557f03cdcfd2cfe2df6ec46fae9a5f
Gowtham Sundrasekaran <gowtham.s@kggroup.in>
2025-07-04 07:28:43 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // import VoiceCallForm from './Components/VoiceCallForm'
 // import UploadAnnouncement from './Components/UploadAnnouncement'
 // import VoiceCallGatewayApp from "./Components/profile"
-import NetworkStatusChecker from './Components/NetworkStatusChecker'
-import APIPage from "./Components/API"
+import NetworkStatusChecker from './Components/NetworkStatusChecker';
+import APIPage from "./Components/API";
+import Dashboard from './Components/Dashboard';
+import "../src/App.css"
 
 const App = () => {
   return (
@@ -12,6 +14,7 @@ const App = () => {
       <NetworkStatusChecker></NetworkStatusChecker>
       <Routes>
         <Route path="/" element={<APIPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         {/* <Route path="/upload" element={<UploadAnnouncement />} />
         <Route path="/profile" element={< VoiceCallForm/>} />
         <Route path="/apiui" element={< VoiceCallGatewayApp/>} /> */}
